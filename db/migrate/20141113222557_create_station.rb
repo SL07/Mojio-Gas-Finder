731665595dbea850193cfda10b189b7f830d8172
Sean Liu <seanliu8207@gmail.com>
2014-11-13 14:52:35 -0800

    Added DB Migration create_station

---
+++ db/migrate/20141113222557_create_station.rb
@@ -0,0 +1,12 @@
+class CreateStation < ActiveRecord::Migration
+  def change
+    create_table :stations do |t|
+      t.string :name
+      t.string :address
+      t.string :city
+      t.decimal :longitude
+      t.decimal :latitude
+      t.decimal :price
+    end
+  end
+end
