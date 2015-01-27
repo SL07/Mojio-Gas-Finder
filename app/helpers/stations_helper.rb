362e9e8e5b5930aa519662e8373cdd1ca0d64cec
Sean Liu <seanliu8207@gmail.com>
2015-01-27 13:53:17 -0800

    added getStationAddressAll in app/helpers/stations_helper. This function is used for finding all gas stations address. To access this method from view: StationsHelper::getStationAddressAll

--- app/helpers/stations_helper.rb
+++ app/helpers/stations_helper.rb
@@ -5,5 +5,12 @@ module StationsHelper
       Station.create!(row.to_hash)
     end
   end
+
+  def getStationAddressAll
+    @stations = Station.select("address")    
+    @stations.map(&:address).join "\n"
+  end
+
 module_function :readCSV
+module_function :getStationAddressAll
 end
