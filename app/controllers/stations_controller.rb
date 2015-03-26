8b74ec2c17110a4dab35914899cff67454963a7e
Sean Liu <seanliu8207@gmail.com>
2015-03-25 18:38:57 -0700

    Remvoed Login from navbar, fixed last Updated Time in station. Deleted all helper function.

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,14 +1,8 @@
 class StationsController < ApplicationController
-  before_action :set_station, only: [:show, :edit, :update, :destroy]
 
-  # GET /stations
-  # GET /stations.json
   def index
     @stations = Station.last(70)
-    @jobs= Job.select("timestamp").last
+    @jobs= Job.select(:timestamp).last
   end
 
-  # GET /stations/new
-  def new
-  end
 end
