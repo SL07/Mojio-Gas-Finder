46db96de626d4e74ee1db065a1a04b5a56eac18a
csx773 <csx773@gmail.com>
2015-03-28 20:35:36 -0700

    testing login with global variables in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,12 +16,30 @@
 
 //var isLogin = false;
 
+var isLive = false;
 var isLoggedIn = false; //for mojioLogin() and mojioLogout() 
+var App, MojioClient, config, mojio_client;
+var redirectURL = 'https://mojio.herokuapp.com/map';  //to be changed 
+
+config = {
+            application: '434f01ce-504b-4f72-a919-5a81dee7d506', // Fill in your app id here!
+            redirect_uri: redirectURL, // Fill in you redirect uri here! (Ex. 'http://localhost:4093/index.html')
+            hostname: 'api.moj.io',
+            version: 'v1',
+            port: '443',
+            scheme: 'https',
+            live: isLive // This will connect your app to the sandbox environment, change it to true to go live. 
+        };
 
 function changeLogoutIcon(){
-  if(isLoggedIn == true)
+  if(isLoggedIn == true) {
     $("#loginButton").replaceWith("<a id=\"logoutButton\" onClick=\"\">Logout</a>");
-  else
-    $("#logoutButton").replaceWith("<a id=\"loginButton\" onClick=\"\">Login</a>");
+    redirectURL = 'https://mojio.herokuapp.com';
+  }
+  else {
+  	$("#logoutButton").replaceWith("<a id=\"loginButton\" onClick=\"\">Login</a>");
+    redirectURL = 'https://mojio.herokuapp.com/map';
+  }
+    
 }
 
